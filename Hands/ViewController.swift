9f33517a81cb208c1cdfd0912294379df6ff66ed
user124196 <gregg.newport@gmail.com>
2017-03-17 11:08:58 -0400

    My First Commit

--- Hands/ViewController.swift
+++ Hands/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var mylabel: UILabel!
+        var tapCount = 0
+   
+    @IBAction func buttonpressed(_ sender: Any) {
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            mylabel.text = ("You've pressed the button 10 times!")
+        
+            }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
